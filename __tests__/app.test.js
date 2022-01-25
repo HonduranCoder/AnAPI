45d2cf0e1849ca2cd553b36ed9124bd1fef5ae70
Estefani <stef45689@gmail.com>
2022-01-25 15:24:07 -0600

    complete with passing tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Barbie = require('../lib/models/Barbie.js');
 
 describe('backend routes', () => {
   beforeEach(() => {
@@ -10,5 +11,85 @@ describe('backend routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should create a barbie', async () => {
+    const res = await request(app).post('/api/v1/barbies').send({
+      name: 'Barbie2',
+      skinColor: 'tan',
+      hairColor: 'blonde',
+      eyeColor: 'green',
+      outfit: 'jumpsuit',
+    });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: 'Barbie2',
+      skinColor: 'tan',
+      hairColor: 'blonde',
+      eyeColor: 'green',
+      outfit: 'jumpsuit',
+    });
+  });
+
+  it('should get a barbie by id', async () => {
+    const barbie = await Barbie.insert({
+      name: 'Barbie3',
+      skinColor: 'porcelain',
+      hairColor: 'blonde',
+      eyeColor: 'hazel',
+      outfit: 'ballerina',
+    });
+    const res = await request(app).get('/api/v1/barbies/1');
+    expect(res.body).toEqual(barbie);
+  });
+
+  it('should list all of the barbies', async () => {
+    const barbie4 = await Barbie.insert({
+      name: 'Barbie4',
+      skinColor: 'Espresso',
+      hairColor: 'black',
+      eyeColor: 'brown',
+      outfit: 'engineer',
+    });
+    const barbie5 = await Barbie.insert({
+      name: 'Barbie4',
+      skinColor: 'Sand',
+      hairColor: 'red',
+      eyeColor: 'green',
+      outfit: 'dress',
+    });
+    const res = await request(app).get('/api/v1/barbies');
+    expect(res.body).toEqual([barbie4, barbie5]);
+  });
+
+  it('should update a dog by id', async () => {
+    const barbie = await Barbie.insert({
+      name: 'Barbie',
+      skinColor: 'brown',
+      hairColor: 'black',
+      eyeColor: 'brown',
+      outfit: 'dress',
+    });
+    const res = await request(app)
+      .patch('/api/v1/barbies/1')
+      .send({ hairColor: 'purple' });
+
+    expect(res.body).toEqual({
+      ...barbie,
+      hairColor: 'purple',
+    });
+  });
+  it('should delete a barbie by id', async () => {
+    const noBarbie = await Barbie.insert({
+      name: 'Ken',
+      skinColor: 'golden',
+      hairColor: 'blonde',
+      eyeColor: 'blue',
+      outfit: 'suit',
+    });
+    const res = await request(app).delete(`/api/v1/barbies/${noBarbie.id}`);
+    expect(res.body).toEqual(noBarbie);
+    expect(await Barbie.getById(noBarbie.id)).toBeNull();
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,15 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+-- Use this file to define your SQL tables
+-- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS barbies;
+
+CREATE TABLE barbies (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name TEXT NOT NULL,
+  skin_color TEXT NOT NULL,
+  hair_color TEXT NOT NULL, 
+  eye_color TEXT NOT NULL, 
+  outfit TEXT NOT NULL
+);
